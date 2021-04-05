5da139f35a5ea90db163fbb73399c971920dc0fe
Tim Brown <timb@Tims-MacBook-Air.local>
2021-04-05 23:30:09 +0100

    v1.5.11

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 1.5.10 
+VERSION = 1.5.11 
 PREFIX_DIR = $(PREFIX)
 ETC = /etc/hoppy
 SHARE = $(PREFIX_DIR)/usr/local/share/hoppy
--- lib/hopclass.py
+++ lib/hopclass.py
@@ -10,7 +10,7 @@
 	
 	Copyright (C) 14/03/2007 - deanx <RID[at]portcullis-secuirty.com>
 	
-	Version 1.5.10
+	Version 1.5.11
 	
 	* This program is free software; you can redistribute it and/or modify
 	* it under the terms of the GNU General Public License as published by
